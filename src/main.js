026b5462c7e7ece905909ca53c4e8707a52e122a
chiwenhui <chi244@126.com>
2022-08-06 22:42:29 +0800

    第三次

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import { createApp } from "vue";
 import App from "./App";
 import router from "./router";
 
-
 // import ElementPlus from "element-plus";
 // import "element-plus/dist/index.css";
-
-
 
 createApp(App)
   .use(router)
